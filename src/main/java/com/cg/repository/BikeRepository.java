97a9001d7c8f878b63d55a732065965389a64e7b
Teja <tejaruttala241@gmail.com>
2023-10-20 09:35:59 +0530

    Repository interface commit

---
+++ src/main/java/com/cg/repository/BikeRepository.java
@@ -0,0 +1,11 @@
+package com.cg.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.cg.entity.Bike;
+
+public interface BikeRepository extends JpaRepository<Bike,Integer>{
+
+	 
+	}
+
+
